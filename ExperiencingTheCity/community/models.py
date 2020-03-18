24fafea605375e70166cc7ab4c19a1391e4e9f9e
Mina <mina.dursun@vektora.com>
2020-03-18 19:22:39 +0300

    initialization of the project (static and templates folders have been added)

---
+++ ExperiencingTheCity/community/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+#Community              -->  Mina        : 18.03.2020 23.00
+#PostType               -->  Mina        : 18.03.2020 23.00
+#Post                   -->  Batı        : 19.03.2020 23.00
+#SemanticTags           -->  Batı        : 19.03.2020 23.00
+#MemberShip             -->  Batı        : 19.03.2020 23.00
+#comments               -->  Burcu       : 20.03.2020 23.00
+#Inappropriate Posts    -->  Burcu       : 20.03.2020 23.00
+#Notification           -->  Adil        : 19.03.2020 12.00
+#UserAdditionalInfo     -->  Adil        : 19.03.2020 12.00
+#Followership           -->  Adil        : 19.03.2020 12.00
+
+
+# + Burcu DB oluşturacak : 20.03.2020 23.00
+
+
+
+
