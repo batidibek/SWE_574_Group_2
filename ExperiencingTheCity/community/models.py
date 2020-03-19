e08e871a39e49f15fc41300d3c9d276ef7d23f0a
Adil_Aghayev <adilaghayev@gmail.com>
2020-03-19 11:53:12 +0300

    models: Notification, UserAdditionalInfo, Followership models have been added in models.py
    Models have been added but still need some clarification on commented parts.

--- ExperiencingTheCity/community/models.py
+++ ExperiencingTheCity/community/models.py
@@ -29,10 +29,31 @@ class PostType(models.Model):
 #MemberShip             -->  Batı        : 19.03.2020 23.00
 #comments               -->  Burcu       : 20.03.2020 23.00
 #Inappropriate Posts    -->  Burcu       : 20.03.2020 23.00
-#Notification           -->  Adil        : 19.03.2020 12.00
-#UserAdditionalInfo     -->  Adil        : 19.03.2020 12.00
-#Followership           -->  Adil        : 19.03.2020 12.00
 
+#Notification           -->  Adil        : 19.03.2020 12.00
+class Notification(models.Model):
+    #type enum?
+    community_id    = models.ForeignKey(Community, default="", on_delete=models.CASCADE)
+    #post_id         = models.ForeignKey(Post, default="", on_delete=models.CASCADE)
+    #user_id vs owner?
+    user_id         = models.ForeignKey(User.pk)
+    description     = models.CharField(max_length=200)
+    # URL of the related event?
+    url             = models.URLField()
+    creation_date   = models.DateTimeField('date published')
+
+#UserAdditionalInfo     -->  Adil        : 19.03.2020 12.00
+class UserAdditionalInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.URLField(max_length=600, blank=True)
+    #geolocation
+
+#Followership           -->  Adil        : 19.03.2020 12.00
+class Followership(models.Model):
+    # user_id vs owner?
+    user_id = models.ForeignKey(User.pk)
+    #clarify
+    follower = models.ManyToManyField(User)
 
 # + Burcu DB oluşturacak : 20.03.2020 23.00
 
