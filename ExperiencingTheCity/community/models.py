28c7413bd02cf1c17930c4ea8c5db0e403b395d8
Mina <mina.dursun@vektora.com>
2020-03-19 09:43:39 +0300

    models: Community and PostType models have been added in models.py

--- ExperiencingTheCity/community/models.py
+++ ExperiencingTheCity/community/models.py
@@ -1,11 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 # Create your models here.
 
-
-
 #Community              -->  Mina        : 18.03.2020 23.00
+class Community(models.Model):
+    name            = models.CharField(max_length=100)
+    description     = models.CharField(max_length=200)
+    creation_date   = models.DateTimeField('date published')
+    active          = models.BooleanField(default=True)
+    owner           = models.ForeignKey(User)
+    # geolocation = models.CharField
+
+
 #PostType               -->  Mina        : 18.03.2020 23.00
+class PostType(models.Model):
+    name            = models.CharField(max_length=100)
+    description     = models.CharField(max_length=200)
+    creation_date   = models.DateTimeField('date published')
+    formfields      = JSONField(default="")
+    community_id    = models.ForeignKey(Community, default="", on_delete=models.CASCADE)
+    owner           = models.ForeignKey(User)
+    complaint       = models.BooleanField(default=False)
+
+
 #Post                   -->  Batı        : 19.03.2020 23.00
 #SemanticTags           -->  Batı        : 19.03.2020 23.00
 #MemberShip             -->  Batı        : 19.03.2020 23.00
