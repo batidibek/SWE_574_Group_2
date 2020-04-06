37e34cf32682a5a04cb276853fe1fe96f6176de9
Batı Dibek <bati@fireflyon.com>
2020-04-06 11:59:12 +0300

    added models in admin.py

--- ExperiencingTheCity/community/admin.py
+++ ExperiencingTheCity/community/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Community, PostType, Post, SemanticTags, MemberShip, Comments, InappropriatePosts, Notification, UserAdditionalInfo, Followership
+
+admin.site.register(Community)
+admin.site.register(PostType)
+admin.site.register(Post)
+admin.site.register(SemanticTags)
+admin.site.register(MemberShip)
+admin.site.register(Comments)
+admin.site.register(InappropriatePosts)
+admin.site.register(Notification)
+admin.site.register(UserAdditionalInfo)
+admin.site.register(Followership)
