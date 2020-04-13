5bace2e3a0eb0b2d61973caaa7ff5b95aea769c5
Batı Dibek <bati@fireflyon.com>
2020-04-13 11:36:06 +0300

    added community list to urls

--- ExperiencingTheCity/community/urls.py
+++ ExperiencingTheCity/community/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('sign-up/', users.sign_up, name='sign_up'),
     path('sign-in/', users.sign_in, name='sign_in'),
     path('new-community/', community.new_community, name='new_community'),
+    path('communities/', community.community_list, name='community_list'),
 
     #REQUESTS
     path('create-user/', users.create_user, name='create_user'),
