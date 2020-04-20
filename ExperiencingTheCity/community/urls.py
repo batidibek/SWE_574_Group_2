027e9d516df30cfbb0ee1a6a3b8241603cd4f886
Mina <mina.dursun@vektora.com>
2020-04-20 14:06:37 +0300

    minad: communites tabı ve post type creation

--- ExperiencingTheCity/community/urls.py
+++ ExperiencingTheCity/community/urls.py
@@ -1,18 +1,20 @@
 from django.urls import path
 from .views import home, users, community
 
-
 app_name = 'community'
 urlpatterns = [
 
-    #PAGES
+    # PAGES
     path('', home.homepage, name='home'),
     path('sign-up/', users.sign_up, name='sign_up'),
     path('sign-in/', users.sign_in, name='sign_in'),
     path('new-community/', community.new_community, name='new_community'),
     path('communities/', community.community_list, name='community_list'),
+    path('communities/<id>', community.getCommunity, name="communityDetail"),
+    path('communities/postType/<id>', community.getCommunityHeader, name="communityDetail"),
+    path('newPostType', community.newPostType),
 
-    #REQUESTS
+    # REQUESTS
     path('create-user/', users.create_user, name='create_user'),
     path('authenticate-user/', users.authenticate_user, name='authenticate_user'),
     path('log-out/', users.log_out, name='log_out'),
--- ExperiencingTheCity/community/views/community.py
+++ ExperiencingTheCity/community/views/community.py
@@ -1,9 +1,10 @@
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
-from ..models import Community, PostType, Post, SemanticTags, MemberShip, Comments, InappropriatePosts, Notification, UserAdditionalInfo, Followership, City
+from ..models import Community, PostType, Post, SemanticTags, MemberShip, Comments, InappropriatePosts, Notification, \
+    UserAdditionalInfo, Followership, City
 from django.http import Http404
 from django.urls import reverse
 import datetime
@@ -15,28 +16,52 @@ from django.core.files import File
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 import requests
+from django.utils import timezone
 
 # COMMUNITY LIST
 
 def community_list(request):
-    community_list = Community.objects.order_by('-pub_date')[:30]
+    community_list = Community.objects.order_by('-creation_date')[:30]
     context = {'community_list': community_list}
     if request.user.is_authenticated:
         community_user = get_object_or_404(UserAdditionalInfo, user=request.user)
         context["user"] = community_user
     return render(request, 'Communities.html', context)
 
+
+def getCommunity(request, id):
+    communityDetail = get_object_or_404(Community, pk=id)
+    communityDataTypes = PostType.objects.filter(community_id=id)
+    communityPosts = Post.objects.filter(community_id=id)
+    print(SemanticTags.objects.filter(community_id=id, post_id=0))
+    tmpObj = serializers.serialize("json", SemanticTags.objects.filter(community_id=id, post_id=0).only("tag_info"))
+    print("---------------------------------")
+    print(tmpObj)
+
+    communityTags = json.loads(tmpObj)
+    return render(request, "CommunityDetail.html", {"communityDetail": communityDetail,
+                                                    "communityDataTypes": communityDataTypes,
+                                                    "communityTags": communityTags,
+                                                    "communityPosts": communityPosts})
+
+
+def getCommunityHeader(request, id):
+    communityDetail = get_object_or_404(Community, pk=id)
+    return render(request, "PostType.html", {"communityDetail": communityDetail})
+
+
 # NEW COMMUNITY
 
-def new_community(request):    
+def new_community(request):
     if not request.user.is_authenticated:
         community_list = Community.objects.order_by('-pub_date')[:30]
         return render(request, 'Home.html', {
             'error_message': "You need to Log in or Sign up to create new community.",
         })
-    else:   
+    else:
         community_user = get_object_or_404(UserAdditionalInfo, user=request.user)
         return render(request, 'CommunityCreate.html', {'user': community_user})
+
 
 def create_community(request):
     print(request.POST)
@@ -45,7 +70,7 @@ def create_community(request):
         return render(request, 'Home.html', {
             'community_list': community_list,
             'error_message': "You need to Log in or Sign up to create new community.",
-        }) 
+        })
     name = str(request.POST.get('name', "")).strip()
     description = str(request.POST.get('description', "")).strip()
     city = str(request.POST.get('city', "")).strip()
@@ -71,7 +96,8 @@ def create_community(request):
             })
     except:
         city_object = City(name=city, country=country)
-    community = Community(name=name, description=description, creation_date=datetime.datetime.now(), active=True, owner=request.user, city=city_object)
+    community = Community(name=name, description=description, creation_date=datetime.datetime.now(), active=True,
+                          owner=request.user, city=city_object)
     if community.name == "" or community.description == "" or request.POST['tags'] == "":
         return render(request, 'CommunityCreate.html', {
             'error_message': "Name, Description or Tag fields cannot be empty.",
@@ -82,3 +108,24 @@ def create_community(request):
         city_object.save()
         community.save()
         return HttpResponseRedirect(reverse('community:home'))
+
+
+## NEW POST TYPE
+
+def newPostType(request):
+    fieldJson = request.POST.get("postTypeFields", "")
+    print(fieldJson)
+    communityId = request.POST.get("communityId", "")
+
+    pt = PostType()
+    pt.community_id = Community.objects.get(pk=communityId)
+    pt.name = request.POST.get("PostTypeName", "")
+    pt.description = request.POST.get("PostTypeDescription", "")
+    pt.owner_id = User.objects.get(username=request.user).id
+    pt.formfields = fieldJson
+    pt.creation_date = timezone.now()
+    pt.complaint = False;
+    pt.save()
+
+    # return HttpResponse(dt.pk)
+    return redirect('/communities/' + communityId)
